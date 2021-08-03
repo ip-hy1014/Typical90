ab8a7ff434cd603b7fb1ce38fd8fe9ffab2ebf4a
hy <81627289+ip-hy1014@users.noreply.github.com>
2021-08-03 19:42:58 +0900

    Add files via upload

---
+++ 020.py
@@ -0,0 +1,9 @@
+#a < c^b の判定
+a,b,c = map(int,input().split())
+if a < c**b:
+  print("Yes")
+else:
+  print("No")
+
+#類題 ABC169C, PANASONIC2020-C
+#https://twitter.com/e869120/status/1385001057512693762/photo/1
